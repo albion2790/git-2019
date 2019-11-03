9b99b4019269f09b79555734197cd671af54582e
albion2790 <54589115+albion2790@users.noreply.github.com>
2019-11-03 22:27:46 +0800

    Add files via upload

---
+++ Management System/src/management/system/Mainframe.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package management.system;
+import javax.swing.JFrame;
+/**
+ *
+ * @author TP048347
+ */
+public class Mainframe extends JFrame {
+   public Mainframe() {
+       super("Management System");
+       
+       setSize(600,500);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setVisible(true);
+   }
+}
